424a532cec6ae5e312b4b893717f01f395ae8b0d
smarsili <smarsili@its.jnj.com>
2023-01-25 13:01:54 +0100

    add test_get_ligands

--- test/basic_tests.py
+++ test/basic_tests.py
@@ -1,4 +1,5 @@
 from pprep.io import read_pdb
+from pprep.utils import get_ligands
 
 PDB_FILE='1J4R.pdb'
 
@@ -8,8 +9,14 @@ def test_read_pdb():
     assert pdb.n_atoms == 2795
 
 
+def test_get_ligands():
+    pdb = read_pdb(PDB_FILE)
+    assert len(get_ligands(pdb)) == 3
+
+
 def main():
     test_read_pdb()
+    test_get_ligands()
         
 
 if __name__ == '__main__':
