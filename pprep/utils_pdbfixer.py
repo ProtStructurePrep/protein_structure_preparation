feff385d0c55d50b2811ea00bd473685b9938db0
Eva <eva.martin.lopez@alum.esci.upf.edu>
2023-01-30 09:58:49 +0100

    added prepare_protein

--- pprep/utils_pdbfixer.py
+++ pprep/utils_pdbfixer.py
@@ -21,3 +21,51 @@ def quick_fix(pdbid):
     fixer.addSolvent(fixer.topology.getUnitCellDimensions())
     PDBFile.writeFile(fixer.topology, fixer.positions, open(pdbid + 'corrected' + '.pdb', 'w'))
     return fixer
+
+def prepare_protein(
+    pdb_file, ignore_missing_residues=True, ignore_terminal_missing_residues=True, ph=7.0
+):
+    """
+    Use pdbfixer to prepare the protein from a PDB file. Hetero atoms such as ligands are
+    removed and non-standard residues replaced. Missing atoms to existing residues are added.
+    Missing residues are ignored by default, but can be included.
+
+    Parameters
+    ----------
+    pdb_file: pathlib.Path or str
+        PDB file containing the system to simulate.
+    ignore_missing_residues: bool, optional
+        If missing residues should be ignored or built.
+    ignore_terminal_missing_residues: bool, optional
+        If missing residues at the beginning and the end of a chain should be ignored or built.
+    ph: float, optional
+        pH value used to determine protonation state of residues
+
+    Returns
+    -------
+    fixer: pdbfixer.pdbfixer.PDBFixer
+        Prepared protein system.
+    """
+    fixer = pdbfixer.PDBFixer(str(pdb_file))
+    fixer.removeHeterogens()  # co-crystallized ligands are unknown to PDBFixer
+    fixer.findMissingResidues()  # identify missing residues, needed for identification of missing atoms
+
+    # if missing terminal residues shall be ignored, remove them from the dictionary
+    if ignore_terminal_missing_residues:
+        chains = list(fixer.topology.chains())
+        keys = fixer.missingResidues.keys()
+        for key in list(keys):
+            chain = chains[key[0]]
+            if key[1] == 0 or key[1] == len(list(chain.residues())):
+                del fixer.missingResidues[key]
+
+    # if all missing residues shall be ignored ignored, clear the dictionary
+    if ignore_missing_residues:
+        fixer.missingResidues = {}
+
+    fixer.findNonstandardResidues()  # find non-standard residue
+    fixer.replaceNonstandardResidues()  # replace non-standard residues with standard one
+    fixer.findMissingAtoms()  # find missing heavy atoms
+    fixer.addMissingAtoms()  # add missing atoms and residues
+    fixer.addMissingHydrogens(ph)  # add missing hydrogens
+    return fixer
