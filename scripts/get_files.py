4909a5bca3946306f69a924525621423885a0ff8
Eva <eva.martin.lopez@alum.esci.upf.edu>
2023-02-09 09:20:07 +0100

    create outputs directory

--- scripts/get_files.py
+++ scripts/get_files.py
@@ -14,7 +14,7 @@ def get_files(pdb_name):
     ligands = md.select_ligands(pdb)
     dist = md.compute_distance_chain_ligand(pdb, protein_chains, ligands)
     ligand_chain = md.associate_ligand_to_chain(dist)
-    md.save_chain_ligand_pdb(pdb_name, pdb, ligands,protein_chains, ligand_chain)
+    md.save_chain_ligand_pdb(pdb_name, pdb, ligands,protein_chains, ligand_chain, "outputs")
     return("Files created")
 
 for i in sys.argv[1:]:
