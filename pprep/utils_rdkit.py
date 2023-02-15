9b8369bf4b7d95e0c46381b4d87640ffd8cef7fb
Eva <eva.martin.lopez@alum.esci.upf.edu>
2023-02-15 16:00:04 +0100

    v1

---
+++ pprep/utils_rdkit.py
@@ -0,0 +1,59 @@
+import copy 
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+
+def prepare_ligand(pdb_file, resname, smiles, depict=True):
+    """
+    Prepare a ligand from a PDB file via adding hydrogens and assigning bond orders. A depiction
+    of the ligand before and after preparation is rendered in 2D to allow an inspection of the
+    results. Huge thanks to @j-wags for the suggestion.
+
+    Parameters
+    ----------
+    pdb_file: pathlib.PosixPath
+       PDB file containing the ligand of interest.
+    resname: str
+        Three character residue name of the ligand.
+    smiles : str
+        SMILES string of the ligand informing about correct protonation and bond orders.
+    depict: bool, optional
+        show a 2D representation of the ligand
+
+    Returns
+    -------
+    prepared_ligand: rdkit.Chem.rdchem.Mol
+        Prepared ligand.
+    """
+    # split molecule
+    rdkit_mol = Chem.MolFromPDBFile(str(pdb_file))
+    rdkit_mol_split = Chem.rdmolops.SplitMolByPDBResidues(rdkit_mol)
+
+    # extract the ligand and remove any already present hydrogens
+    ligand = rdkit_mol_split[resname]
+    ligand = Chem.RemoveHs(ligand)
+
+    # assign bond orders from template
+    reference_mol = Chem.MolFromSmiles(smiles)
+    prepared_ligand = AllChem.AssignBondOrdersFromTemplate(reference_mol, ligand)
+    prepared_ligand.AddConformer(ligand.GetConformer(0))
+
+    # protonate ligand
+    prepared_ligand = Chem.rdmolops.AddHs(prepared_ligand, addCoords=True)
+    
+  
+    # 2D depiction
+    if depict:
+        ligand_2d = copy.deepcopy(ligand)
+        prepared_ligand_2d = copy.deepcopy(prepared_ligand)
+        AllChem.Compute2DCoords(ligand_2d)
+        AllChem.Compute2DCoords(prepared_ligand_2d)
+        display(
+            Draw.MolsToGridImage(
+                [ligand_2d, prepared_ligand_2d], molsPerRow=2, legends=["original", "prepared"]
+            )
+        )
+        
+    
+    # return ligand
+    return prepared_ligand
