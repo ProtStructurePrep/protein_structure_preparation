60bef080ca077bd83de40e02b3fdf8c00c79eda6
Eva <eva.martin.lopez@alum.esci.upf.edu>
2023-02-28 15:34:27 +0100

    changes function name

--- test/test_10_pdbs.py
+++ test/test_10_pdbs.py
@@ -3,7 +3,7 @@ import pprep.utils_mdtraj as md
 PDB_NAMES = ['1j4r', '2fv9', '2jfz', '3fnu', '3qs1', '3kdb', '2bua', '2buc', '3ckr', '3cw8']
 EXPECTED_LIGANDS = ['001','INN','003','006','006','006','007','008','009','00A']
 
-def test_ligand():
+def test_select_ligand():
     l = []
     for i in range(len(PDB_NAMES)):
         print(PDB_NAMES[i])
@@ -13,7 +13,7 @@ def test_ligand():
         assert ligand == EXPECTED_LIGANDS[i]
 
 def main(): 
-    test_ligand()
+    test_select_ligand()
 
 
 if __name__ == '__main__':
