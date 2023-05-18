c99af7e1b12925c4fd71d7587b39bc9d0f954d5f
evamartin1240 <eva.martin.lopez@alum.esci.upf.edu>
2023-05-18 10:33:49 +0200

    return parmed object in prepare_system

--- pprep/MDutils.py
+++ pprep/MDutils.py
@@ -33,62 +33,6 @@ def load_prepared_receptor(receptor_file):
     """
     receptor = app.PDBFile(receptor_file)
     return receptor
-    
-def prepare_ligand(pdb_file, resname, smiles, depict=True):
-    """
-    Extracted from: https://projects.volkamerlab.org/teachopencadd/talktorials/T019_md_simulation.html
-    
-    Prepare a ligand from a PDB file via adding hydrogens and assigning bond orders. A depiction
-    of the ligand before and after preparation is rendered in 2D to allow an inspection of the
-    results. Huge thanks to @j-wags for the suggestion.
-
-    Parameters
-    ----------
-    pdb_file: string
-       PDB file containing the ligand of interest.
-    resname: str
-        Three character residue name of the ligand.
-    smiles : str
-        SMILES string of the ligand informing about correct protonation and bond orders.
-    depict: bool, optional
-        show a 2D representation of the ligand
-
-    Returns
-    -------
-    prepared_ligand: rdkit.Chem.rdchem.Mol
-        Prepared ligand.
-    """
-    # split molecule
-    rdkit_mol = Chem.MolFromPDBFile(str(pdb_file))
-    rdkit_mol_split = Chem.rdmolops.SplitMolByPDBResidues(rdkit_mol)
-
-    # extract the ligand and remove any already present hydrogens
-    ligand = rdkit_mol_split[resname]
-    ligand = Chem.RemoveHs(ligand)
-
-    # assign bond orders from template
-    reference_mol = Chem.MolFromSmiles(smiles)
-    prepared_ligand = AllChem.AssignBondOrdersFromTemplate(reference_mol, ligand)
-    prepared_ligand.AddConformer(ligand.GetConformer(0))
-
-    # protonate ligand
-    prepared_ligand = Chem.rdmolops.AddHs(prepared_ligand, addCoords=True)
-    
-    # 2D depiction
-    if depict:
-        ligand_2d = copy.deepcopy(ligand)
-        prepared_ligand_2d = copy.deepcopy(prepared_ligand)
-        AllChem.Compute2DCoords(ligand_2d)
-        AllChem.Compute2DCoords(prepared_ligand_2d)
-        display(
-            Draw.MolsToGridImage(
-                [ligand_2d, prepared_ligand_2d], molsPerRow=2, legends=["original", "prepared"]
-            )
-        )
-        
-    
-    # return ligand
-    return prepared_ligand
 
 def prepare_system(receptor, ligand, solvate=False):
     """ 
@@ -107,7 +51,7 @@ def prepare_system(receptor, ligand, solvate=False):
     -------
     system: openmm.openmm.System
         Prepared complex system for further simulations
-    complex_structure: parmed.structure.Structure
+    parmed_structure: parmed.structure.Structure
         Topology structure of the complex (with both the receptor and the ligand)
     """
     
@@ -134,10 +78,10 @@ def prepare_system(receptor, ligand, solvate=False):
         openff_mol.to_topology().to_openmm(), xyz=openff_mol.conformers[0]
     )
 
-    complex_structure = parmed_receptor + parmed_ligand
+    parmed_structure = parmed_receptor + parmed_ligand
     
     
-    modeller = app.Modeller(complex_structure.topology, complex_structure.positions)
+    modeller = app.Modeller(parmed_structure.topology, parmed_structure.positions)
     if solvate:
         # Solvate
         modeller.addSolvent(system_generator.forcefield, model='tip3p', padding=5.0*unit.angstroms, ionicStrength=0.15*unit.molar)
@@ -145,9 +89,9 @@ def prepare_system(receptor, ligand, solvate=False):
     else:
         system = system_generator.create_system(modeller.topology)
 
-    return system, modeller
+    return system, modeller, parmed_structure
     
-def setup_openmm_simulation(system, complex_structure, time_step=1*unit.femtoseconds, temperature=300*unit.kelvin, friction=1/unit.picosecond, platform_name="CPU"):
+def setup_openmm_simulation(system, modeller, time_step=1*unit.femtoseconds, temperature=300*unit.kelvin, friction=1/unit.picosecond, platform_name="CPU"):
     """
     Propagate the System with Langevin dynamics and set up the openmm simulation.
     
@@ -155,7 +99,7 @@ def setup_openmm_simulation(system, complex_structure, time_step=1*unit.femtosec
     ----------
     system: openmm.openmm.System
         Prepared complex system for further simulations
-    complex_structure: parmed.structure.Structure
+    modeller: 
         Topology structure of the complex (with both the receptor and the ligand)
     time_step: openmm.unit.quantity.Quantity
         Simulaion timestep with default value set to 1 * unit.femtoseconds 
@@ -178,10 +122,10 @@ def setup_openmm_simulation(system, complex_structure, time_step=1*unit.femtosec
 
     # set up an openmm simulation
     simulation = app.Simulation(
-        complex_structure.topology, system, integrator_min, platform=platform
+        modeller.topology, system, integrator_min, platform=platform
     )
     
-    simulation.context.setPositions(complex_structure.positions)
+    simulation.context.setPositions(modeller.positions)
     simulation.context.setVelocitiesToTemperature(temperature)
     
     return simulation
